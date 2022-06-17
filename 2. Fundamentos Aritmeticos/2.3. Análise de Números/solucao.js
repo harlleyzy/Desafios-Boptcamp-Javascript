1a9715d46b7c0c6e06036887c16694b5aea06fe0
Carolyne Sobreira Ferreira <105233274+harlleyzy@users.noreply.github.com>
2022-06-17 10:30:02 -0300

    analise-de-números
    corrigido!

--- 2. Fundamentos Aritmeticos/2.3. Análise de Números/solucao.js
+++ 2. Fundamentos Aritmeticos/2.3. Análise de Números/solucao.js
@@ -1,18 +1,48 @@
+let valoresPares = 0;
 
-let numero = Array(5);
+let valoresImpares = 0;
+
+let valoresPositivos = 0;
+
+let valoresNegativos = 0;
 
 
-for(let i =0 ; i < numero.length; i++){
-    numero[i] = parseInt(gets());
+for (var i = 0; i < 5; i++) {
+
+ const valorInformadoPeloUsuario = parseInt(gets());
+
+
+ // TODO: Complete os espaços em branco com uma solução possível para o problema.
+
+ if (valorInformadoPeloUsuario%2==0) {
+
+   valoresPares++;
+
+ } else {
+
+   valoresImpares++;
+
+ }
+
+
+ if (valorInformadoPeloUsuario > 0) {
+
+   valoresPositivos++;
+
+ } else if (valorInformadoPeloUsuario < 0) {
+
+   valoresNegativos++;
+
+ }
+
+
 }
 
-let positivos = numero.filter(value => value > 0);
-let negativos = numero.filter(value => value < 0);
-let pares = numero.filter(value => value % 2 == 0);
-let impares = numero.filter(value => value % 2 != 0);
 
-console.log(pares.length + " valor(es) par(es)");
-console.log(impares.length + " valor(es) impar(es)");
-console.log(positivos.length + " valor(es) positivo(s)");
-console.log(negativos.length + " valor(es) negativo(s)");
+console.log(`${valoresPares} par(es)`);
 
+console.log(`${valoresImpares} impar(es)`);
+
+console.log(`${valoresPositivos} positivo(s)`);
+
+console.log(`${valoresNegativos} negativo(s)`);
